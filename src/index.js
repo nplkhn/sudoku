6588806cc93024790c24aee307d7b2f384ebb40f
Nikita Plakhin <nplkhn1@yandex.ru>
2019-02-26 23:14:23 +0300

    solution found

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  const matrixCopy = matrix;
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (matrixCopy[row][col] === 0) {
+        const options = count(row, col, matrixCopy);
+        for (let option of options) {
+          matrixCopy[row][col] = option;
+          solveSudoku(matrixCopy);
+        }
+      }
+    }
+  }
+  return matrixCopy;
+}
+
+function count(row, col, matrix) {
+  const options = [];
+
+  row = help(row) * 3;
+  col = help(col) * 3;
+
+  for (let i = 0; i < 9; i++) {
+    options.push([matrix[row][i], matrix[i][col], matrix[row + i % 3][col + help(i)]])
+  }
+
+  return options;
+}
+
+function help(num) {
+  return Math.floor(num / 3);
 }
